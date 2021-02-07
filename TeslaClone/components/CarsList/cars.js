9a1e2d358f3ccee8dcaa1605c88d48b372c282d0
Vivek Bhardwaj <vivekb153@gmail.com>
2021-02-06 19:23:20 -0500

    final commit

--- TeslaClone/components/CarsList/cars.js
+++ TeslaClone/components/CarsList/cars.js
@@ -4,17 +4,17 @@ export default [{
   image: require('../../assets/images/ModelS.jpeg'),
 }, {
   name: 'Model 3',
-  tagline: 'Order Online for',
+  tagline: 'Order Online for ',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/Model3.jpeg'),
 }, {
   name: 'Model X',
-  tagline: 'Order Online for',
+  tagline: 'Order Online for ',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/ModelX.jpeg'),
 }, {
   name: 'Model Y',
-  tagline: 'Order Online for',
+  tagline: 'Order Online for ',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/ModelY.jpeg'),
 }];
